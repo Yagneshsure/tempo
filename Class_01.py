97cc8de1d634a1a32dda78c309303e3e227b2834
yagnesh sure <107101522+Yagneshsure@users.noreply.github.com>
2024-07-03 13:20:28 +0530

    Add files via upload

---
+++ Class_01.py
@@ -0,0 +1,38 @@
+# Data types
+
+# # String
+# name = 'ushsree'
+# print(name)
+
+# # integer 
+# num1 = 7
+# print(type(num1))
+
+# num2 = "100"
+# print(type(num2))
+
+
+# ### Float
+# num1 = 100.00
+# print(type(num1))
+
+## Boolyns
+
+# valu = True
+# print(type(valu))
+
+
+## Input statments
+# name = input("Please Enter Your Name: ")
+# opp = input("Please Enter Your Opinion: ")
+# print(name)
+# print(opp)
+
+# add = 5 + 6
+# print(add)
+
+# sub = 5 - 6
+# print(sub)
+
+# div = 5/6
+# print(div)
